31a1046cdba71482c7597edfe5edefc9ab7387cb
Steven Phan <steven.phan009@gmail.com>
2021-12-30 21:08:14 -0800

    added camera follow and extended level

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
@@ -10,7 +8,7 @@ public class FollowPlayer : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         Debug.Log(player.position);     
         transform.position = player.position + offset;
     }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb; 
     public float forwardForce = 2000f; 
-    public float sidewayForce = 200f; 
+    public float sidewayForce = 750f; 
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +20,10 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(0,0,forwardForce*Time.deltaTime);  
 
         if(Input.GetKey("d")){
-            rb.AddForce(sidewayForce*Time.deltaTime,0,0); 
+            rb.AddForce(sidewayForce*Time.deltaTime,0,0,ForceMode.VelocityChange); 
         }
         if(Input.GetKey("a")){
-            rb.AddForce(-sidewayForce*Time.deltaTime,0,0); 
+            rb.AddForce(-sidewayForce*Time.deltaTime,0,0,ForceMode.VelocityChange); 
             
         }
     }
