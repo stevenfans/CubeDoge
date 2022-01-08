ee525659f3b5378df5aa292acf41de632bf16c56
Steven Phan <steven.phan009@gmail.com>
2022-01-08 12:36:12 -0800

    spawn ahead z axis

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -5,6 +5,7 @@ public class BlockSpawner : MonoBehaviour
     public Transform[] spawnPoints; 
     public GameObject block;
     public Transform player; 
+    private float z; 
 
     // Start is called before the first frame update
     void Start()
@@ -12,6 +13,9 @@ public class BlockSpawner : MonoBehaviour
         int randomIndex = Random.Range(0,spawnPoints.Length); 
 
         for(int i = 0; i<spawnPoints.Length; i++){
+            // z = spawnPoints[i].position.z; 
+            // z = z +10;
+            spawnPoints[i].transform.position = new Vector3(spawnPoints[i].position.x,spawnPoints[i].position.y,spawnPoints[i].position.z+20); 
             if(randomIndex!=i){
                 Instantiate(block,spawnPoints[i].position,Quaternion.identity); 
             }
