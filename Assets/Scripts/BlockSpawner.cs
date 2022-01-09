5673df20d8c41da94945740e096e33a8145cdb58
Steven Phan <steven.phan009@gmail.com>
2022-01-08 21:13:27 -0800

    procedural block inifite loop

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -5,27 +5,33 @@ public class BlockSpawner : MonoBehaviour
     public Transform[] spawnPoints; 
     public GameObject block;
     public Transform player; 
-    private float z; 
+    private float spawnTime = 2f; 
 
     // Start is called before the first frame update
     void Start()
     {
-        int randomIndex = Random.Range(0,spawnPoints.Length); 
-
-        for(int i = 0; i<spawnPoints.Length; i++){
-            // z = spawnPoints[i].position.z; 
-            // z = z +10;
-            spawnPoints[i].transform.position = new Vector3(spawnPoints[i].position.x,spawnPoints[i].position.y,spawnPoints[i].position.z+20); 
-            if(randomIndex!=i){
-                Instantiate(block,spawnPoints[i].position,Quaternion.identity); 
-            }
-        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > spawnTime){
+            spawnTime = Time.time + 5;
+            SpawnBlock(); 
+        } 
+    }
+
+    void SpawnBlock()
+    {
+        int randomIndex = Random.Range(0,spawnPoints.Length); 
+
+        for(int i = 0; i<spawnPoints.Length; i++){
+           
+            if(randomIndex!=i){ 
+                spawnPoints[i].position =  new Vector3(spawnPoints[i].position.x,spawnPoints[i].position.y,player.position.z+50); 
+                Instantiate(block,spawnPoints[i].position,Quaternion.identity); 
+            }
+        }
     }
 }
